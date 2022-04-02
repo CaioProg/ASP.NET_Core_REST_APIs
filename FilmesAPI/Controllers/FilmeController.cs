7d2e294d8de70ebacf3fa93d1414ba2f3f176db5
CaioProg <caioprog18@gmail.com>
2022-04-02 16:30:36 -0300

    Recebendo dados de um filme

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,20 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController] // classe para criar a API
+    [Route("[controller]")] // define q a rota para acessa a API será sempre 'nome' + 'Controller', ou seja, FilmeController
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+       
+        [HttpPost] 
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
