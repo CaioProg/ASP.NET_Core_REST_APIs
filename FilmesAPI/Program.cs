97f590f76b7983e6a416d79529e7dd88e7226f51
CaioProg <caioprog18@gmail.com>
2022-04-02 19:37:23 -0300

    Gerando a primeira migration

--- FilmesAPI/Program.cs
+++ FilmesAPI/Program.cs
@@ -1,11 +1,32 @@
 using FilmesAPI;
+using FilmesAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var startup = new Startup(builder.Configuration);
-startup.ConfigureServices(builder.Services);
+string FilmeConnection = builder.Configuration.GetConnectionString("FilmeConnection");
+
+builder.Services.AddDbContextPool<FilmeContext>(options =>
+                options.UseMySql(FilmeConnection,
+                      ServerVersion.AutoDetect(FilmeConnection)));
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();   
 
 var app = builder.Build();
 
-startup.Configure(app, app.Environment);
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
 app.Run();
